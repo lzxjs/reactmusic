07c6e49c542757f30e806da1317e26bd45c02ee0
liuzixuan <76858586@qq.com>
2019-12-18 14:07:17 +0800

    完成路由配置

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { GlobalStyle } from  './style';
+import { IconStyle } from './assets/iconfont/iconfont';
+import { renderRoutes } from 'react-router-config';//renderRoutes 读取路由配置转化为 Route 标签
+import routes from './routes/index.js';
+import { HashRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <HashRouter>
+      <GlobalStyle></GlobalStyle>
+      <IconStyle></IconStyle>
+      { renderRoutes (routes) }
+    </HashRouter>
   );
 }
 
