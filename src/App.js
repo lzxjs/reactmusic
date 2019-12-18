0a766f0244f06d639a910e0b7e0785a73d7fcc26
liuzixuan <76858586@qq.com>
2019-12-18 15:34:27 +0800

    完成redux搭建和swiper

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import { GlobalStyle } from  './style';
-import { IconStyle } from './assets/iconfont/iconfont';
-import { renderRoutes } from 'react-router-config';//renderRoutes 读取路由配置转化为 Route 标签
-import routes from './routes/index.js';
+import React from 'react'
+import { Provider } from 'react-redux'
+import { GlobalStyle } from  './style'
+import { renderRoutes } from 'react-router-config'
+import { IconStyle } from './assets/iconfont/iconfont'
+import store from './store/index'
+import routes from './routes/index.js'
 import { HashRouter } from 'react-router-dom';
 
-function App() {
+function App () {
   return (
-    <HashRouter>
-      <GlobalStyle></GlobalStyle>
-      <IconStyle></IconStyle>
-      { renderRoutes (routes) }
-    </HashRouter>
-  );
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        { renderRoutes (routes) }
+      </HashRouter>
+    </Provider>
+  )
 }
 
 export default App;
